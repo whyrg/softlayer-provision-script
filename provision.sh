86d110ca445ad7479ddc1b0a8517854147442a16
Richard Guo <r.guo@aerospike.com>
2017-11-15 15:51:14 -0800

    Added EE support

--- provision.sh
+++ provision.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+set -x
+
+version=${ASD_VERSION:-latest}
+
+
 apt-get update 
 
 apt-get install curl -y
 
-curl -L -s  https://www.aerospike.com/download/server/latest/artifact/ubuntu16 -o aerospike-server.tgz
+if [[ -n "${EE_USER}" ]] && [[ -n "${EE_PASS}" ]]; then
+curl -L -s --user ${EE_USER}:${EE_PASS} https://www.aerospike.com/enterprise/download/server/${version}/artifact/ubuntu16 -o aerospike-server-enterprise.tgz
+else 
+curl -L -s  https://www.aerospike.com/download/server/${version}/artifact/ubuntu16 -o aerospike-server-community.tgz
+fi
 
-tar -C /opt -xzf aerospike-server.tgz 
+tar -C /opt -xzf aerospike-server-*.tgz 
 
 cd /opt/aerospike-server*
 
